e825d2292f525f99182c1c3e9442bc5d08f19fa9
papajin <papajini@gmail.com>
2017-09-26 16:32:43 +0300

    First commit

---
+++ ii_simplified_comment_moderation.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package ii_simplified_comment_moderation
+ * @version 1.0
+ */
+/*
+Plugin Name: Simplified Comment Auto Moderation.
+Plugin URI: http://ihor.ivanets.com/
+Description: WP Plugin for comment auto approval. If comment author's email found with already approved comment, the comment will be approved even if the author's name differs.
+Version: 1.0
+Author URI: http://ihor.ivanets.com/
+*/
+
+
+if (! function_exists('ii_simplified_comment_moderation')) {
+    /**
+     * The function runs almost same query as the native check_comment function
+     * but without comment_author in the where statement.
+     * We are going to re-check only those comments that are not marked spam and not approved yet, i.e. $approved == 0.
+     *
+     * @param $approved
+     * @param $commentdata
+     * @return int result 1 - if such a comment found or still 0 otherwise.
+     */
+    function ii_simplified_comment_moderation( $approved , $commentdata )
+    {
+        if($approved === 0) {
+            global $wpdb;
+            $approved = (int) $wpdb->get_var(
+            	$wpdb->prepare(
+            		"SELECT comment_approved FROM $wpdb->comments WHERE comment_author_email = %s and comment_approved = '1' LIMIT 1",
+		            $commentdata['comment_author_email'] )
+            );
+        }
+
+        return $approved;
+    }
+}
+
+add_filter( 'pre_comment_approved' , 'ii_simplified_comment_moderation' , '99', 2 );
